22ad7a7abad9e2d8f95df4aaae2535d3139d156a
James Wang <jwang2013@gmail.com>
2015-06-15 00:33:55 -0400

    changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :pins
+
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about"
   get "frucket" => "pages#frucket"
 
